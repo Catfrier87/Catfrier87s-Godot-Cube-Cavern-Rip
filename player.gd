5f1a3bd3fe6b855030f26e98ac4e6823c4455a12
One Sin and Hundreds of Good Deeds <catfrier87@gmail.com>
2025-09-29 02:46:58 +0200

    Generic project start
    Basic camera and basic movement

---
+++ player.gd
@@ -0,0 +1,21 @@
+extends RigidBody3D
+
+@export var walkspeed = 1200 ## Per Second
+
+var move_direction := Vector3.ZERO
+
+func _ready():
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		$Neck.rotate_y(-deg_to_rad(event.relative.x * 0.4))
+		$Neck/PlayerCamera.rotate_x(-deg_to_rad(event.relative.y * 0.4))
+		
+
+func _process(delta: float) -> void:
+	move_direction = Vector3.ZERO
+	move_direction.x = Input.get_axis("walk_left", "walk_right")
+	move_direction.z = Input.get_axis("walk_forward", "walk_backward")
+	
+	apply_central_force(move_direction * walkspeed * delta)
